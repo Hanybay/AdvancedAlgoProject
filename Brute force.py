342aa6ccd44baaa8d41a96efa4b4bda64992f3eb
hager-safwat <39493926+hager-safwat@users.noreply.github.com>
2023-11-17 08:21:51 +0100

    Update Brute force.py

--- Brute force.py
+++ Brute force.py
@@ -1,1 +1,30 @@
+A = [[1, 2, -1, -4, -20],
+     [-8, -3, 4, 2, 1]
+     [3, 8, 10, 1, 3],
+     [-4, -1, 1, 7, -6]]
+def max_subarry(A):
+  max_sum =float('-inf')
+  first_row=0
+  last_row =0
+  first_col=0
+  last_col=0
+  rows =le(A)
+  cols =len(A[0])
 
+  for i in range(rows):
+    for j in range(cols):
+      for k in range(i,rows):
+         for l in range(j,cols):
+             current_sum =0
+             for s in range(i, k+1):
+                for o in range(j,l+1):
+                    current_sum += A[s][o]
+
+             if current_sum > max_sum :
+                max_sum = current_sum
+                first_row, last_row, first_col, last_col = i, k , j , l
+
+  return max_sum, first_row, last_row, first_col, last_col 
+  result = max_subarry(A)
+  print("sum:", results[0])
+  print("indeces({},{},{},{})".format(result[1], result[2], result[3],result[4]))

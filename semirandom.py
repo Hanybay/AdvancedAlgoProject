62c8e8af568492c91a09d2852368f1636a9d3819
Ryuma69 <azedine.amirouche@gmail.com>
2023-12-02 16:35:57 +0100

    final version semi-brute-force

--- semirandom.py
+++ semirandom.py
@@ -24,7 +24,7 @@ def findtheoricbound(A):
     mean_neg = abs(negativ)/m_negativ
     mean_pos = positiv/n_positiv
 
-    return max, min, positiv, abs(negativ), mean_pos, mean_neg
+    return mean_pos, mean_neg
 
 
 def calculate_submatrix_sum(matrix, start_row, start_col, height, width):
@@ -47,21 +47,23 @@ def calculate_all_submatrix_sums(matrix, height, width):
     cols = len(matrix[0])
     submatrix_sums = []
 
-    for i in range(rows - height + 1):
-        for j in range(cols - width + 1):
+    for i in range(rows - width + 1):
+        for j in range(cols - height + 1):
             start_row = i
             start_col = j
-            submatrix_sum = calculate_submatrix_sum(
-                matrix, start_row, start_col, height, width)
+            if start_row != 0 and start_col != 0:
+                submatrix_sum = calculate_submatrix_sum(
+                    matrix, start_row, start_col, height, width)
 
             # Ajoute une liste à la liste principale
-            submatrix_sums.append([start_row, start_col, submatrix_sum])
+                submatrix_sums.append(
+                    [start_row, start_col, height, width, submatrix_sum])
 
     return submatrix_sums
 
 
 def maxsubmatrixsum(matrix):
-    max, min, sumpositiv, sumnegativ, mean_positiv, mean_negativ = findtheoricbound(
+    mean_positiv, mean_negativ = findtheoricbound(
         matrix)
 
     width = len(matrix[0])
@@ -69,22 +71,44 @@ def maxsubmatrixsum(matrix):
     boundwidth = width//2
     boundheight = height//2
     tmpmaxsum = -1000
+    temp_res = []
     if mean_positiv > mean_negativ:
-
-        for i in range(height-1, 0, -1):
-            for j in range(width-1, 0, -1):
-                temp_res = calculate_all_submatrix_sums(
-                    matrix, boundheight+i, boundwidth+j)
+        print("we there")
+        for i in range(height-1, 1, -1):
+            for j in range(width-1, 1, -1):
+                temp_res.append(calculate_all_submatrix_sums(
+                    matrix, boundheight+i-1, boundwidth+j-1))
 
     else:
-
-        for i in range(boundheight):
-            for j in range(boundwidth):
-                temp_res = calculate_all_submatrix_sums(
-                    matrix, i, j)
+        print("we here")
+        for i in range(1, boundheight, 1):
+            for j in range(1, boundwidth, 1):
+                temp_res.append(calculate_all_submatrix_sums(
+                    matrix, i, j))
 
     print(temp_res)
+    for k in temp_res:
+        for l in k:
+            if l[4] > tmpmaxsum:
+                tmpmaxsum = l[4]
+                result = l
+
+    return result
 
 
-maxsubmatrixsum(A)
-maxsubmatrixsum(B)
+solution = maxsubmatrixsum(A)
+
+print("Max sum for A : {}, coord {} ".format(
+    solution[4], (solution[0], solution[1], solution[2], solution[3])))
+print(A[0])
+print(A[1])
+print(A[2])
+print(A[3])
+solution2 = maxsubmatrixsum(B)
+
+print("Max sum for B : {}, coord {} ".format(
+    solution2[4], (solution2[0], solution2[1], solution2[2], solution2[3])))
+print(B[0])
+print(B[1])
+print(B[2])
+print(B[3])
